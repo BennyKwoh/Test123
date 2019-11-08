894247a58f9882fa217af2fac917e4b969c8131d
bekwoh <bekwoh@LSG0100247.corp.capgemini.com>
2019-11-08 12:21:43 +0800

    Hello

---
+++ SonarCube/src/main/java/SonarQube.java
@@ -0,0 +1,11 @@
+
+public class SonarQube {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello");
+System.out.println("Bye");
+System.out.println("SonarQube");
+	}
+
+}
